febfde2843fcb04b7ad12736748d3e7055d754bb
Sangharsh Aglave <samcoolasever@gmail.com>
2017-02-22 11:19:25 +0530

    jsdoc annotations added

--- www/MASStoragePlugin.js
+++ www/MASStoragePlugin.js
@@ -53,22 +53,58 @@ module.exports = MASStoragePlugin = {
          */
     MASSecureLocalStorage: function () {
 		'use strict';
+
+        /**
+        *   Saves the provided data to local storage
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} mime mime type of the data
+        *   @param {string} key key with which the data is to be stored
+        *   @param {string} data data payload
+        *   @param {string} mode mode in which the data is to be saved
+        */
         this.save = function (successHandler, errorHandler, mime, key, data, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "saveToLocal", [mime, key, data, mode]);
         };
 
+        /**
+        *   Find data using key and mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} key key of the data to be searched
+        *   @param {string} mode mode in which the data was saved
+        */
         this.findByUsingKeyAndMode = function (successHandler, errorHandler, key, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "findByUsingKeyAndModeLocal", [key, mode]);
         };
 
+        /**
+        *   Delete data using key and mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} key key of the data to be deleted
+        *   @param {string} mode mode in which the data was saved
+        */
         this.deleteByUsingKeyAndMode = function (successHandler, errorHandler, key, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "deleteByUsingKeyAndModeLocal", [key, mode]);
         };
 
+        /**
+        *   Delete all data in a mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} mode mode for which the data needs to be deleted
+        */
         this.deleteAllUsingMode = function (successHandler, errorHandler, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "deleteAllUsingModeLocal", [mode]);
         };
 
+        /**
+        *   Returns the sets of keys for a given mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} mode mode for which the key set is required
+        */
         this.keySetForMode = function (successHandler, errorHandler, mode) {
 			return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "keySetForModeLocal", [mode]);
         };
@@ -79,18 +115,48 @@ module.exports = MASStoragePlugin = {
          */
     MASSecureCloudStorage: function () {
 		'use strict';
+
+        /**
+        *   Saves the provided data to cloud storage
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} mime mime type of the data
+        *   @param {string} key key with which the data is to be stored
+        *   @param {string} data data payload
+        *   @param {string} mode mode in which the data is to be saved
+        */
         this.save = function (successHandler, errorHandler, mime, key, data, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "saveToCloud", [mime, key, data, mode]);
         };
 
+        /**
+        *   Find data using key and mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} key key of the data to be searched
+        *   @param {string} mode mode in which the data was saved
+        */
         this.findByUsingKeyAndMode = function (successHandler, errorHandler, key, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "findByUsingKeyAndModeCloud", [key, mode]);
         };
 
+        /**
+        *   Delete data using key and mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} key key of the data to be deleted
+        *   @param {string} mode mode in which the data was saved
+        */
         this.deleteByUsingKeyAndMode = function (successHandler, errorHandler, key, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "deleteByUsingKeyAndModeCloud", [key, mode]);
         };
 
+        /**
+        *   Returns the sets of keys for a given mode
+        *   @param {function} successHandler user defined success callback
+        *   @param {function} errorHandler user defined error callback
+        *   @param {string} mode mode for which the key set is required
+        */
         this.keySetForMode = function (successHandler, errorHandler, mode) {
             return Cordova.exec(successHandler, errorHandler, "MASStoragePlugin", "keySetForModeCloud", [mode]);
         };
